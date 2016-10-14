aa19f2aeb198e79a70a24ef46e02b593f79bcc62
Makerprojects <Glitzi85>
2016-10-14 06:57:42 +0200

    Supports PiKoder/SSC 2.06 and updated .NET Framework (1.0.4.4)

--- PCC PiKoder Control Center/PCC_PiKoder_Control_Center.vb
+++ PCC PiKoder Control Center/PCC_PiKoder_Control_Center.vb
@@ -755,7 +755,7 @@ ErrorExit:
                 Call mySerialLink.GetFirmwareVersion(strChannelBuffer)
                 If strChannelBuffer <> "TimeOut" Then
                     strSSC_Firmware.Text = strChannelBuffer
-                    If Val(strChannelBuffer) > 2.03 Then
+                    If Val(strChannelBuffer) > 2.06 Then
                         MsgBox("The PiKoder firmware version found is not supported! Please goto www.pikoder.com and upgrade PCC Control Center to the latest version.", MsgBoxStyle.OkOnly, "Error Message")
                         End
                     ElseIf Val(strChannelBuffer) < 2.0 Then
