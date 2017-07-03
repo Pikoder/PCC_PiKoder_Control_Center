78edee6d1ea1558de2f401a0639304a0d1b126ec
gregor <gregor@pikoder.com>
2017-07-03 21:19:56 +0200

    Minimal change accepting UART2PPM 2.05 now (1.051)

--- PCC PiKoder Control Center/My Project/AssemblyInfo.vb
+++ PCC PiKoder Control Center/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.7")> 
-<Assembly: AssemblyFileVersion("1.0.5.0")> 
+<Assembly: AssemblyVersion("0.0.0.8")> 
+<Assembly: AssemblyFileVersion("1.0.5.1")> 
